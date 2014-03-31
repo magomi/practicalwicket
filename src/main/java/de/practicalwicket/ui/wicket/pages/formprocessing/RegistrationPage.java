955685cca79e7ed0a8750c66bae675d74320fa6b
bert <taseroth@gmail.com>
2014-03-31 22:38:02 +0200

    simple form processing. validation

--- src/main/java/de/practicalwicket/ui/wicket/pages/formprocessing/RegistrationPage.java
+++ src/main/java/de/practicalwicket/ui/wicket/pages/formprocessing/RegistrationPage.java
@@ -2,12 +2,14 @@ package de.practicalwicket.ui.wicket.pages.formprocessing;
 
 import de.practicalwicket.model.Registration;
 import de.practicalwicket.ui.wicket.pages.general.BasePage;
-import org.apache.wicket.markup.html.form.CheckBox;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.PasswordTextField;
-import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.form.*;
+import org.apache.wicket.markup.html.form.validation.EqualPasswordInputValidator;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.validation.IValidatable;
+import org.apache.wicket.validation.IValidator;
+import org.apache.wicket.validation.ValidationError;
 
 /**
  * Created by bert
@@ -18,13 +20,17 @@ public class RegistrationPage extends BasePage {
     private String passwordRepeat;
 
     public RegistrationPage() {
-        super("Login");
+        super("Registration");
     }
 
 
     @Override
     protected void onInitialize() {
         super.onInitialize();
+
+
+        FeedbackPanel feedbackPanel = new FeedbackPanel("feedback");
+        add(feedbackPanel);
 
         Form<Registration> form = new Form<Registration>("form"
                 , new CompoundPropertyModel<Registration>(new Registration())) {
@@ -38,10 +44,31 @@ public class RegistrationPage extends BasePage {
         };
 
         add(form);
-        form.add(new TextField<String>("email"));
-        form.add(new TextField<String>("userName"));
-        form.add(new PasswordTextField("password"));
-        form.add(new PasswordTextField("passwordRepeat", new PropertyModel<String>(this, "passwordRepeat")));
+        TextField<String> email = new EmailTextField("email");
+        email.setRequired(true);
+
+        form.add(email);
+        TextField<String> userName = new TextField<String>("userName");
+        userName.add(new IValidator<String>() {
+            @Override
+            public void validate(IValidatable<String> validatable) {
+                if (validatable.getValue().length() < 4 ) {
+                    ValidationError validationError = new ValidationError().addKey("username.error");
+                    validationError.setVariable("userName", validatable.getValue());
+                    validatable.error(validationError);
+                }
+            }
+        });
+        form.add(userName);
+
+        PasswordTextField passwordField = new PasswordTextField("password");
+        form.add(passwordField);
+        PasswordTextField passwordRepeatField
+                = new PasswordTextField("passwordRepeat", new PropertyModel<String>(this, "passwordRepeat"));
+        passwordRepeatField.setRequired(true);
+        form.add(passwordRepeatField);
+        form.add(new EqualPasswordInputValidator(passwordField, passwordRepeatField));
+
         form.add(new CheckBox("spam"));
 
     }
